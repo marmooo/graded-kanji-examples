5e8b7f8b43f4077449d74a4ae28c606b1bdf912e
marmooo <marmooo@users.noreply.github.com>
2024-08-20 07:32:48 +0900

    bump onkun from 0.2.6 to 0.2.8

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 import { readLines } from "https://deno.land/std/io/mod.ts";
-import { Onkun } from "https://raw.githubusercontent.com/marmooo/onkun/v0.2.6/mod.js";
+import { Onkun } from "https://raw.githubusercontent.com/marmooo/onkun/v0.2.8/mod.js";
 import { YomiDict } from "https://raw.githubusercontent.com/marmooo/yomi-dict/v0.1.8/mod.js";
 import { JKAT } from "npm:@marmooo/kanji@0.0.8";
 
