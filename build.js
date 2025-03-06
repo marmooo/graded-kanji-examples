5cdaa03f60d46b3463504fc4a14b3e864780a789
marmooo <marmooo@users.noreply.github.com>
2025-03-07 04:01:35 +0900

    bump SudachiDict from v20241021 to v20250129

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 import { Onkun } from "npm:onkun@0.3.0";
-import { YomiDict } from "npm:yomi-dict@0.2.0";
+import { YomiDict } from "npm:yomi-dict@0.2.1";
 import { JKAT } from "npm:@marmooo/kanji@0.0.8";
 
 async function getGradedWords(filePath, kanji) {
